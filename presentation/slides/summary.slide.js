7f1269401d673d89adab177d213595d1a80ed284
Joe Fehrman <joseph.fehrman@gmail.com>
2018-11-06 20:40:30 -0500

    Presentation summary slide.

---
+++ presentation/slides/summary.slide.js
@@ -0,0 +1,22 @@
+// Import React
+import React from "react";
+
+// Import Spectacle Core tags
+import {
+  Slide,
+  Heading,
+  List,
+  ListItem
+} from "spectacle";
+
+export default () => (
+  <Slide transition={["fade"]} bgColor="primary" textColor="tertiary">
+    <Heading size={6} textColor="secondary" caps>What is Storybook?</Heading>
+    <List textColor="quaternary">
+      <ListItem>UI Development Environment</ListItem>
+      <ListItem>Visual Testing Tool</ListItem>
+      <ListItem>Living Style Documentation</ListItem>
+      <ListItem>Deliverable Value</ListItem>
+    </List>
+  </Slide>
+);
