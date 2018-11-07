94eb130293dbf55ea4c5efee3aa08bdbf8952412
Joe Fehrman <joseph.fehrman@gmail.com>
2018-11-06 20:40:00 -0500

    Created first slide.

---
+++ presentation/slides/title.slide.js
@@ -0,0 +1,20 @@
+// Import React
+import React from "react";
+
+// Import Spectacle Core tags
+import {
+  Heading,
+  Slide,
+  Text
+} from "spectacle";
+
+export default () => (
+  <Slide transition={["zoom"]} bgColor="primary">
+    <Heading size={1} fit caps lineHeight={1} textColor="secondary">
+      Storybook
+    </Heading>
+    <Text margin="10px 0 0" textColor="tertiary" size={1} fit bold>
+      Component Based Happily Ever After
+    </Text>
+  </Slide>
+);
