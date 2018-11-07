5a8833d3309d4bddf0c29a344a9608c6882d5d8b
Joe Fehrman <joseph.fehrman@gmail.com>
2018-11-06 20:40:10 -0500

    Created personal intro slide.

---
+++ presentation/slides/intro.slide.js
@@ -0,0 +1,19 @@
+// Import React
+import React from "react";
+import ProfilePicture from "../../assets/joe-beard.jpg";
+
+// Import Spectacle Core tags
+import {
+  Heading,
+  Slide,
+  Image
+} from "spectacle";
+
+export default () => (
+  <Slide transition={["fade"]} bgColor="primary">
+    <Heading size={5} caps lineHeight={1} textColor="secondary">
+      Hi I'm Joe!
+    </Heading>
+    <Image src={ProfilePicture} />
+  </Slide>
+);
